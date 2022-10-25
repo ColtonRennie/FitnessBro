5dde9e8f591f25d6b4eca4f52956961a5a1a4d15
Colton Rennie <colton.rennie@hotmail.com>
2022-10-25 20:19:11 -0300

    Able to delete any food that are in your food index

--- main_app/urls.py
+++ main_app/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('food/create/', views.FoodCreate.as_view(), name='food_create'),
     path('food/', views.food_index, name='food_index'),
     path('food/<int:pk>/update/', views.FoodUpdate.as_view(), name='food_update'),
+    path('food/<int:pk>/delete/', views.FoodDelete.as_view(), name='food_delete'),
 ]
